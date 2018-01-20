bbffc426035b0f526f9db070cf7d4ea044854ba0
Robert Saylor <robert@customphpdesign.com>
2018-01-20 14:37:28 -0500

    user favorites

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -37,24 +37,24 @@ class UserController extends Controller
         $email = $session->get('email');
         $id = $session->get('id');
 
-        if(isset($id)) {
-            $sql = "SELECT `first`,`last`,`email`,`phone` FROM `users` WHERE `id` = '$id' AND `active` = 'Yes' LIMIT 1";
-            $result = $em->getConnection()->prepare($sql);
-            $result->execute();
-            $data = "";
-            while ($row = $result->fetch()) {
-                foreach ($row as $key=>$value) {
-                    $data[$key] = $value;
-                }
-            }
-
-            return $this->render('user/profile.html.twig',[
-                'data' => $data,
-            ]);
-        } else {
+        if(!isset($id)) {
             $this->addFlash('danger','Your session has expired. Please log back in.');
-            return $this->redirectToRoute('homepage');  
+            return $this->redirectToRoute('homepage');
         }
+
+        $sql = "SELECT `first`,`last`,`email`,`phone` FROM `users` WHERE `id` = '$id' AND `active` = 'Yes' LIMIT 1";
+        $result = $em->getConnection()->prepare($sql);
+        $result->execute();
+        $data = "";
+        while ($row = $result->fetch()) {
+            foreach ($row as $key=>$value) {
+                $data[$key] = $value;
+            }
+        }
+
+        return $this->render('user/profile.html.twig',[
+            'data' => $data,
+        ]);
     } 
 
     /**   
@@ -65,6 +65,11 @@ class UserController extends Controller
         $em = $this->getDoctrine()->getManager();
         $session = $this->get('commonservices')->GetSessionData();
         $id = $session->get('id');
+
+        if(!isset($id)) {
+            $this->addFlash('danger','Your session has expired. Please log back in.');
+            return $this->redirectToRoute('homepage');
+        }
 
         $first = $request->request->get('first');
         $last = $request->request->get('last');
@@ -94,4 +99,110 @@ class UserController extends Controller
         return $this->redirectToRoute('updateprofile'); 
 
     }
+
+    /**   
+     * @Route("/addfavorite", name="addfavorite")
+     */
+    public function addfavoriteAction(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $session = $this->get('commonservices')->GetSessionData();
+        $id = $session->get('id');
+
+        if(!isset($id)) {
+            $this->addFlash('danger','Your session has expired. Please log back in.');
+            return $this->redirectToRoute('homepage');
+        }
+
+        $Matrix_Unique_ID = $request->query->get('Matrix_Unique_ID');
+        $today = date("Ymd");
+        $sql = "INSERT INTO `favorites` 
+        (`Matrix_Unique_ID`,`userID`,`date_added`) 
+        VALUES
+        ('$Matrix_Unique_ID','$id','$today')
+        ";
+        $result = $em->getConnection()->prepare($sql);
+        $result->execute(); 
+
+        $this->addFlash('success','The property has been added to your favorites. Click on your dashboard to manage.');
+        return $this->redirectToRoute('details',[
+            'id' => $Matrix_Unique_ID,
+        ]);
+    }
+
+    /**   
+     * @Route("/myfavorite", name="myfavorite")
+     */
+    public function myfavoriteAction(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $session = $this->get('commonservices')->GetSessionData();
+        $id = $session->get('id');
+
+        if(!isset($id)) {
+            $this->addFlash('danger','Your session has expired. Please log back in.');
+            return $this->redirectToRoute('homepage');
+        }
+
+        $data = "";
+
+        $sql = "
+        SELECT
+            `f`.`id`,
+            `p`.`Matrix_Unique_ID`,
+            `p`.`ListPrice`,
+            `p`.`Address`,
+            `p`.`City`,
+            `p`.`StateOrProvince`,
+            `p`.`PostalCode`
+
+        FROM
+            `favorites` f
+
+        LEFT JOIN `properties` p ON `f`.`Matrix_Unique_ID` = `p`.`Matrix_Unique_ID`
+
+        WHERE
+            `f`.`userID` = '$id'
+
+        ORDER BY `f`.`date_added` ASC
+        ";
+
+        $result = $em->getConnection()->prepare($sql);
+        $result->execute(); 
+        $i = "0";
+        while ($row = $result->fetch()) {
+            foreach ($row as $key=>$value) {
+                $data[$i][$key] = $value;
+            }
+            $i++;
+        }
+
+        return $this->render('user/myfavorite.html.twig',[
+            'data' => $data,
+        ]);
+    }
+
+    /**   
+     * @Route("/deletefavorite", name="deletefavorite")
+     */
+    public function deletefavoriteAction(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $session = $this->get('commonservices')->GetSessionData();
+        $id = $session->get('id');
+
+        if(!isset($id)) {
+            $this->addFlash('danger','Your session has expired. Please log back in.');
+            return $this->redirectToRoute('homepage');
+        }
+
+        $Matrix_Unique_ID = $request->query->get('Matrix_Unique_ID');
+
+        $sql = "DELETE FROM `favorites` WHERE `Matrix_Unique_ID` = '$Matrix_Unique_ID' AND `userID` = '$id'";
+        $result = $em->getConnection()->prepare($sql);
+        $result->execute();
+
+        $this->addFlash('success','The property has been removed to your favorites.');
+        return $this->redirectToRoute('myfavorite'); 
+    }               
 }
